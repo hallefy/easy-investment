bfcab3f3127127d597d04cfd290196f5e7f96fc5
Hallefy <hallefy@fretebras.com.br>
2018-10-10 13:53:34 -0300

    Moving application class to root

---
+++ app/src/main/java/ferreira/hallefy/easyinvestment/Application.kt
@@ -0,0 +1,36 @@
+package ferreira.hallefy.easyinvestment
+
+import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import ferreira.hallefy.easyinvestment.di.DaggerApplicationComponent
+
+
+class Application : DaggerApplication() {
+    lateinit var applicaton: Application
+
+    override fun onCreate() {
+        super.onCreate()
+        applicaton = this
+    }
+
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        val appComponent = DaggerApplicationComponent.builder().application(this).build()
+        appComponent.inject(this)
+
+        return appComponent
+    }
+
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    fun getContext(): Application {
+        return applicaton
+    }
+}
