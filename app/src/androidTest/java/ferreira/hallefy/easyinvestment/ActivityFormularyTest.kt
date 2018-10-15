832444e4118e9c69a8fe777f7abcbde9b24c916f
Hallefy Ferreira <hallefy.fc@hotmail.com>
2018-10-15 18:47:38 -0300

    Instrumented test on formulary activity, done.

--- app/src/androidTest/java/ferreira/hallefy/easyinvestment/ActivityFormularyTest.kt
+++ app/src/androidTest/java/ferreira/hallefy/easyinvestment/ActivityFormularyTest.kt
@@ -1,16 +1,27 @@
 package ferreira.hallefy.easyinvestment
 
+import android.content.Context
+import android.provider.Settings.Global.getString
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.intent.ActivityResultFunction
 import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.matcher.IntentMatchers
+import android.support.test.espresso.matcher.ViewMatchers.hasErrorText
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import ferreira.hallefy.easyinvestment.presentation.views.formulary.view.ActivityFormulary
+import ferreira.hallefy.easyinvestment.presentation.views.result.view.ActivityResultInvestiment
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mock
+
+
 
 @RunWith(JUnit4::class)
 class ActivityFormularyTest {
@@ -24,14 +35,36 @@ class ActivityFormularyTest {
         Intents.init()
     }
 
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
     @Test
     fun testSimulationSuccess() {
 
         onView(withId(R.id.inputAplicacao)).perform(typeText("200000"))
         onView(withId(R.id.inputDataVencimento)).perform(typeText("16062020"), scrollTo())
-        onView(withId(R.id.inputPercentualCdi)).perform(typeText("123"), scrollTo())
+        onView(withId(R.id.inputPercentualCdi)).perform(typeText("123"), scrollTo(), closeSoftKeyboard())
 
-        onView(withId(R.id.btnSimular)).perform(click() , scrollTo())
+        onView(withId(R.id.btnSimular)).perform(click())
+
+        IntentMatchers.hasPackage(ActivityResultInvestiment::class.java.`package`.name)
     }
 
+    @Test
+    fun testIfErrorMessagesIsShowed() {
+        onView(withId(R.id.btnSimular)).perform(click())
+
+        onView(withId(R.id.inputAplicacao)).check(matches(hasErrorText("Digite um valor maior que 0")))
+        onView(withId(R.id.inputAplicacao)).perform(typeText("200000"), closeSoftKeyboard())
+
+        onView(withId(R.id.btnSimular)).perform(click())
+        onView(withId(R.id.inputDataVencimento)).check(matches(hasErrorText("Digite uma data válida")))
+        onView(withId(R.id.inputDataVencimento)).perform(typeText("16062020"), scrollTo(), closeSoftKeyboard())
+
+        onView(withId(R.id.btnSimular)).perform(click())
+        onView(withId(R.id.inputPercentualCdi)).check(matches(hasErrorText("Digite um valor maior que 0")))
+        onView(withId(R.id.inputPercentualCdi)).perform(typeText("123"), scrollTo(), closeSoftKeyboard())
+    }
 }
